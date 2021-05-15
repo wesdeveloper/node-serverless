a126b1d7662bdb7df8f3924e4162afa1ccb722c6
wesdeveloper <wesleyalopes2010@gmail.com>
2021-05-15 19:02:17 -0300

    test: add test to counter get amount route

--- test/functional/counter.spec.js
+++ test/functional/counter.spec.js
@@ -8,6 +8,8 @@ const {
 
 describe('Counter Routes', () => {
   describe('Increment route', () => {
+    afterEach(() => nock.cleanAll());
+
     it('Should make an request to increment the access number and receive status code 200', async () => {
       nock(COUNT_API_HOST)
         .get(`/hit/${COUNT_API_NAMESPACE}/${COUNT_API_KEY}`)
@@ -15,6 +17,22 @@ describe('Counter Routes', () => {
 
       const response = await global.request.post('/counter');
       expect(response.status).toBe(200);
+    });
+  });
+
+  describe('Get current amount of access route', () => {
+    afterEach(() => nock.cleanAll());
+
+    it('Should make an request to see the current amount of access', async () => {
+      nock(COUNT_API_HOST)
+        .get(`/get/${COUNT_API_NAMESPACE}/${COUNT_API_KEY}`)
+        .reply(200, {
+          value: 1,
+        });
+
+      const response = await global.request.get('/counter');
+      expect(response.status).toBe(200);
+      expect(response.body.amount).toBe(1);
     });
   });
 });
