c7c2792d43a13145d7d87074180e97e28575ffcb
wesdeveloper <wesleyalopes2010@gmail.com>
2021-05-16 15:53:04 -0300

    feat: add database load on index and packages install

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const logger = require('morgan');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
 
 const healthcheckRoutes = require('./modules/healtcheck/healthcheck-routes');
 const counterRoutes = require('./modules/counter/counter-routes');
+const usersRoutes = require('./modules/users/users-routes');
 
 dotenv.config();
 
 const app = express();
+require('./config/database');
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 healthcheckRoutes(app);
 counterRoutes(app);
+usersRoutes(app);
 
 module.exports = app;
