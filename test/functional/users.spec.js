6f0a378ed9d4035120172b1bdbe11ebc05895c29
wesdeveloper <wesleyalopes2010@gmail.com>
2021-05-16 15:20:56 -0300

    test: create user bad request cases route test

--- test/functional/users.spec.js
+++ test/functional/users.spec.js
@@ -11,5 +11,24 @@ describe('Users Routes', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(userMockData));
     });
+
+    describe('Validations cases', () => {
+      const userMockData = usersFixtures.generateUserMock();
+      Object.keys(userMockData).forEach(key => {
+        const userMock = { ...userMockData };
+        delete userMock[key];
+
+        it(`Should make an request to create a new user without ${key} and receive status code 400`, async () => {
+          const response = await global.request
+            .post('/users')
+            .send(userMock);
+
+          expect(response.status).toBe(400);
+
+          const errorItem = response.body.filter(error => error.path === key);
+          expect(errorItem).not.toBeNull();
+        });
+      });
+    });
   });
 });
